91ddd2b260e42ac1232382602cdc5d219d488f77
Bo Vial <bvial@macquariecloudservices.com>
2023-11-22 12:07:00 +1100

    added bufferline

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -25,19 +25,28 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
--- [[ Set ToggleTerm keymaps ]]
-function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-end
+-- [[ ToggleTerm keymaps ]]
+-- Set it in all windows
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
+vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
+vim.keymap.set({'n', 't'}, '<C-h>', [[<Cmd>wincmd h<CR>]])
+vim.keymap.set({'n', 't'}, '<C-j>', [[<Cmd>wincmd j<CR>]])
+vim.keymap.set({'n', 't'}, '<C-k>', [[<Cmd>wincmd k<CR>]])
+vim.keymap.set({'n', 't'}, '<C-l>', [[<Cmd>wincmd l<CR>]])
+vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
 
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+-- use this to only use in ToggleTerm
+-- function _G.set_terminal_keymaps()
+--   local opts = {buffer = 0}
+--   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+--   vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+--   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+--   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+--   vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+--   vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+--   vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+-- end
+-- 
+-- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 -- vim: ts=2 sts=2 sw=2 et
