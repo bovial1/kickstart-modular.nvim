0f83c0c1ad30735fe430b2c04c9e6e61d5aa8062
Bo Vial <bovial1@gmail.com>
2023-12-04 09:19:25 +1100

    added comment

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,6 +31,7 @@ vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
 -- vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
 vim.keymap.set({'n', 't'}, '<C-h>', [[<Cmd>wincmd h<CR>]])
 vim.keymap.set({'n', 't'}, '<C-j>', [[<Cmd>wincmd j<CR>]])
+-- This conflicts with <C-k> for LSP signature hover. Won't work if LSP is attached to buffer.
 vim.keymap.set({'n', 't'}, '<C-k>', [[<Cmd>wincmd k<CR>]])
 vim.keymap.set({'n', 't'}, '<C-l>', [[<Cmd>wincmd l<CR>]])
 -- vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
