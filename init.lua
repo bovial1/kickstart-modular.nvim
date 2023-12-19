3f703e616febd47ff8cea4ed1631f050496f9354
Bo Vial <bvial@macquariecloudservices.com>
2023-12-19 14:50:14 +1100

    check for vscode

--- init.lua
+++ init.lua
@@ -44,36 +44,40 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- [[ Install `lazy.nvim` plugin manager ]]
-require('lazy-bootstrap')
+if vim.g.vscode then
+    -- VSCode extension
+else
+  -- [[ Install `lazy.nvim` plugin manager ]]
+  require('lazy-bootstrap')
 
--- [[ Configure plugins ]]
-require('lazy-plugins')
+  -- [[ Configure plugins ]]
+  require('lazy-plugins')
 
--- [[ Setting options ]]
-require('options')
+  -- [[ Setting options ]]
+  require('options')
 
--- [[ Basic Keymaps ]]
-require('keymaps')
+  -- [[ Basic Keymaps ]]
+  require('keymaps')
 
--- [[ Configure Telescope ]]
--- (fuzzy finder)
-require('telescope-setup')
+  -- [[ Configure Telescope ]]
+  -- (fuzzy finder)
+  require('telescope-setup')
 
--- [[ Configure Treesitter ]]
--- (syntax parser for highlighting)
-require('treesitter-setup')
+  -- [[ Configure Treesitter ]]
+  -- (syntax parser for highlighting)
+  require('treesitter-setup')
 
--- [[ Configure LSP ]]
--- (Language Server Protocol)
-require('lsp-setup')
+  -- [[ Configure LSP ]]
+  -- (Language Server Protocol)
+  require('lsp-setup')
 
--- [[ Configure nvim-cmp ]]
--- (completion)
-require('cmp-setup')
+  -- [[ Configure nvim-cmp ]]
+  -- (completion)
+  require('cmp-setup')
 
--- [[ My customer setup ]]
-require('custom-setup')
+  -- [[ My customer setup ]]
+  require('custom-setup')
+end
 
 -- [[ Remember to set this in .bashrc ]]
 -- Map ESC to Caps Lock so VIM is easier to use
