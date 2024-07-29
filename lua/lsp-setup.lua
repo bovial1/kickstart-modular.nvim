65e5146d42b13c75ad1e8a79de16d5badafc1799
Bo Vial <bovial1@gmail.com>
2024-07-29 16:00:43 +1000

    update which-key

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -44,15 +44,21 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
+require('which-key').add({
+  {'<leader>c', group = '[C]ode' },
+  {'<leader>d', group = '[D]ocument' },
+  {'<leader>g', group = '[G]it' },
+  {'<leader>h', group = 'More git' },
+  {'<leader>r', group = '[R]ename' },
+  {'<leader>s', group = '[S]earch' },
+  {'<leader>w', group = '[W]orkspace' },
+}, {
+  mode = 'n', -- NORMAL mode
+  prefix = '<leader>',
+  silent = true,
+  noremap = true,
+  nowait = true,
+})
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
